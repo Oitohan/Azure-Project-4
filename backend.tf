cba1652b90dec6397af621c985a63750d83248da
Oitohan <113042402+Oitohan@users.noreply.github.com>
2022-10-14 10:27:19 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -11,7 +11,15 @@ terraform {
 provider "azurerm" {
   features {}
 }
-
+terraform {
+  backend "azurerm" {
+    resource_group_name =
+   "StorageAccount-ResourceGroup"
+     storage_account_name = "abcd1234"
+     container_name = "tfstate"
+    key = "prod.terraform.tfstate"
+  }
+}
 # Create a resource group
 
 resource "azurerm_resource_group" "project4RG" {
